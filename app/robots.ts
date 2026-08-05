4e64a7f0a65ed032aec1a859123c81793910886a
marketopiaagency487-wq <marketopiaagency487@gmail.com>
2026-08-06 02:42:03 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 /* ⚠️ غيّر الدومين هنا لدومينك الفعلي بعد الشراء */
-const SITE_URL = "https://alamelroum-qataridiar.com";
+const SITE_URL = "https://alamelroum-qataridiar.org";
 
 export const dynamic = "force-static";
 
